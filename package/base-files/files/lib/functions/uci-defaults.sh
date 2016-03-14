aa5cdc1d061a4d22f0be09fcb7e17d8e122bff8c
bettermanbao <jackybao_sh@msn.com>
2016-03-14 08:45:15 +0800

    Update uci-defaults.sh

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -213,7 +213,7 @@ set network.lan.ifname='$ifname'
 set network.lan.force_link=1
 set network.lan.type='bridge'
 set network.lan.proto='static'
-set network.lan.ipaddr='192.168.1.1'
+set network.lan.ipaddr='192.168.101.1'
 set network.lan.netmask='255.255.255.0'
 set network.lan.ip6assign='60'
 EOF
@@ -226,9 +226,30 @@ ucidef_set_interface_wan() {
 set network.wan='interface'
 set network.wan.ifname='$ifname'
 set network.wan.proto='dhcp'
-set network.wan6='interface'
-set network.wan6.ifname='$ifname'
-set network.wan6.proto='dhcpv6'
+#set network.wan6='interface'
+#set network.wan6.ifname='$ifname'
+#set network.wan6.proto='dhcpv6'
+EOF
+}
+
+ucidef_set_interface_wan_only() {
+	local ifname=$1
+
+	uci batch <<EOF
+set network.wan='interface'
+set network.wan.ifname='$ifname'
+set network.wan.proto='dhcp'
+#set network.wan6='interface'
+#set network.wan6.ifname='$ifname'
+#set network.wan6.proto='dhcpv6'
+set network.lan='interface'
+#set network.lan.ifname='$ifname'
+set network.lan.force_link=1
+#set network.lan.type='bridge'
+set network.lan.proto='static'
+set network.lan.ipaddr='192.168.101.1'
+set network.lan.netmask='255.255.255.0'
+set network.lan.ip6assign='60'
 EOF
 }
 
